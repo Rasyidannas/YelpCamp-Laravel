fca0d2266301fcd75a9089a67a9bb30bde8c6149
rasyid annas <rasshit.dsgn@gmail.com>
2023-08-19 02:03:24 +0700

    build: changed some files

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,20 +3,11 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
-
 echo "Caching config..."
 php artisan config:cache
-
-echo "Route lists..."
-php artisan route:list
 
 echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-
-echo "Running migrations fresh"
-php artisan migrate:fresh --force
